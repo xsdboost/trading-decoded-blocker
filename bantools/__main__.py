ac2327ea57102f1a92132915bf1093877fc10b99
Steve Volker <xsd.boost@gmail.com>
2022-07-17 12:54:59 -0400

    including tests

--- bantools/__main__.py
+++ bantools/__main__.py
@@ -5,7 +5,7 @@ from bantools.utils.envtools import getenv_variable
 
 def main():
 
-    banningTools = Bot(command_prefix="/")
+    banningTools = Bot()
     banningTools.add_listener(usecase_did_user_already_signup, "on_member_join")
     banningTools.run(getenv_variable("API_KEY"))
 
