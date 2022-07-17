f36a2f9b728bb579a11fcdd6f7abded1deec6cca
Steve Volker <xsd.boost@gmail.com>
2022-07-17 18:15:45 -0400

    move account key request

--- bantools/__main__.py
+++ bantools/__main__.py
@@ -5,11 +5,12 @@ from bantools.utils.envtools import getenv_variable
 
 
 def main():
+    account_key = getenv_variable("API_KEY")
 
-    banningTools = Bot()
-    banningTools.add_listener(usecase_did_user_already_signup, "on_member_join")
-    banningTools.run(getenv_variable("API_KEY"))
+    new_account_validator = Bot(command_prefix="-")
+    new_account_validator.add_listener(usecase_did_user_already_signup, "on_member_join")
 
+    new_account_validator.run(account_key)
 
 if __name__ == "__main__":
     main()
