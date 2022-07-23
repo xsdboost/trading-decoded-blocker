7f86b9b5ba50b504cafc72c00bf8182bd3c82b4b
xsdboost <xsd.boost@gmail.com>
2022-07-22 21:33:39 -0400

    finalized code

--- bantools/repositories/communications.py
+++ bantools/repositories/communications.py
@@ -1,21 +1,41 @@
 from discord import utils, Guild, TextChannel
 from bantools.messaging_content.warning_channel import OfferMessage
 
+
 class ChannelCommunicator:
     def __init__(self, guild: Guild, channel_name: str):
         self.guild: Guild = guild
         self.channel: TextChannel = self._get_channel_by_name(channel_name)
 
-    def _get_channel_by_name(self, channel_name: str):
+    def _get_channel_by_name(self, channel_name: str) -> TextChannel:
+        """
+        Given a channel name retrieves the accompanying TextChannel object
+
+        Parameters
+        ----------
+        channel_name: str
+            The channel we want to retrieve the Textchannel object for
+        Returns
+        -------
+        TextChannel
+            channel in guild that has channel name
+
+        """
         return utils.get(self.guild.channels, name=channel_name)
 
-    @property
-    def channel_name(self):
-        return self.channel.name
+    async def send(self, offend_message: OfferMessage) -> None:
+        """
 
-    @channel_name.setter
-    def channel_name(self, channel_name: str):
-        self.channel = self._get_channel_by_name(channel_name)
+        Method send Text embed objects to discord server
 
-    async def send(self, offend_message: OfferMessage):
+        Parameters
+        ----------
+        offend_message: OfferMessage
+            Embed object to be send by TextChannel
+
+        Returns
+        -------
+        None
+
+        """
         await self.channel.send(embed=offend_message.content)
